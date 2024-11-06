64081a7ec654272521d83388b75aaea32e116cdf
wangjincheng <275375496@qq.com>
2024-11-07 01:25:53 +0800

    feat|> 登录页简单示例

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -9,5 +9,5 @@
 </script>
 
 <style scoped lang="scss">
-@import "./index.scss";
+@use "./index.scss";
 </style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,9 +1,36 @@
 <template>
-  <div>
-    <h1>登录页</h1>
+  <div class="login-page-bg">
+    <div class="main-login-form">
+      <div class="login-form-label">
+        <h2>登录</h2>
+      </div>
+      <form class="login-form">
+        <input
+            maxlength="30"
+            minlength="5"
+            required="true"
+            class="username-input"
+            type="text"
+            placeholder="username"
+        />
+        <input
+            maxlength="30"
+            minlength="5"
+            required="true"
+            class="password-input"
+            type="password"
+            placeholder="password"
+        />
+        <button class="login-button">login</button>
+      </form>
+    </div>
   </div>
 </template>
 
-<style>
+<script setup lang="ts">
 
+</script>
+
+<style scoped lang="scss">
+@use "./index.scss";
 </style>
