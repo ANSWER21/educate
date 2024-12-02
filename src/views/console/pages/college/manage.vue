f4b17e06aa03b4956e1bc730b4a1f100eeef81a1
wangjincheng <275375496@qq.com>
2024-12-02 11:44:44 +0800

    feat|> 院校管理表格及科目抽屉

--- src/views/console/pages/college/manage.vue
+++ src/views/console/pages/college/manage.vue
@@ -26,9 +26,9 @@
           <el-table-column prop="level" label="办学层次"/>
           <el-table-column prop="remark" label="备注"/>
           <el-table-column label="操作">
-            <template #default="examScope">
-              <el-button size="small" @click="handleEdit(examScope.row)">编辑</el-button>
-              <el-button size="small" type="danger" @click="handleDelete(examScope.row.code)">删除</el-button>
+            <template #default="collegeScope">
+              <el-button size="small" @click="handleCollegeEdit(collegeScope.row)">编辑</el-button>
+              <el-button size="small" type="danger" @click="handleCollegeDelete(collegeScope.row.code)">删除</el-button>
             </template>
           </el-table-column>
         </el-table>
@@ -43,17 +43,48 @@
             @current-change="handleCurrentChange"
         />
       </div>
+      <el-drawer
+          v-model="isShow"
+          direction="rtl"
+          :title="college?.name ?? ''"
+      >
+        <div>
+          <!-- 新建科目的表单 -->
+          <el-form ref="subjectForm" :model="subjectForm">
+            <el-form-item label="科目编号">
+              <el-input v-model="subjectForm.code"></el-input>
+            </el-form-item>
+            <el-form-item label="科目名称">
+              <el-input v-model="subjectForm.name"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="addSubject">添加</el-button>
+            </el-form-item>
+          </el-form>
+          <!-- 科目列表表格 -->
+          <el-table stripe border :data="subjects">
+            <el-table-column property="code" label="编号"/>
+            <el-table-column property="name" label="名称"/>
+            <el-table-column label="操作">
+              <template #default="subjectScope">
+                <el-button size="small" type="danger" @click="handleSubjectDelete(subjectScope.row.code)">删除
+                </el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </div>
+      </el-drawer>
     </el-main>
-
   </el-container>
 </template>
 
 <script setup lang="ts">
-import {onMounted, ref} from 'vue';
+import {onMounted, ref, watch} from 'vue';
 import {ElMessage} from 'element-plus';
 import {Search} from '@element-plus/icons-vue'
-import {College, Exam} from '@/models/exam.ts';
-import {getCollegesByPage,} from '@/api/models/exam.ts';
+import {College, Subject} from '@/models/exam.ts';
+import {getCollegesByPage, getSubjectByCollege,} from '@/api/models/exam.ts';
+import {CODE_SUCCESS} from "@/models/resultJson.ts";
 
 const collegeFilterKeyword = ref<string | null>()
 const colleges = ref<College[]>([])
@@ -62,9 +93,17 @@ const currentPage = ref(1);
 const pageSize = ref(10);
 const total = ref(0);
 
+const isShow = ref()
+const college = ref<College>()
+const subjects = ref<Subject[]>([]);
+
 onMounted(() => {
   fetchColleges()
 })
+
+watch(college, () => {
+  fetchSubject()
+});
 
 const fetchColleges = async () => {
   try {
@@ -77,6 +116,24 @@ const fetchColleges = async () => {
   }
 };
 
+const fetchSubject = async () => {
+  if (!college?.value?.code) {
+    subjects.value = [];
+  } else {
+    await getSubjectByCollege(college.value.code)
+        .then((res) => {
+          if (res.code === CODE_SUCCESS) {
+            subjects.value = res.data;
+          } else {
+            ElMessage.error(res.msg);
+          }
+        })
+        .catch((error) => {
+          ElMessage.error(error.message || '请求失败');
+        });
+  }
+}
+
 const handleSizeChange = (newPageSize: number) => {
   pageSize.value = newPageSize;
   fetchColleges();
@@ -87,15 +144,39 @@ const handleCurrentChange = (newPage: number) => {
   fetchColleges();
 };
 
-const handleEdit = (row: Exam) => {
-  ElMessage.info(`编辑: ${row.title}`);
+const handleCollegeEdit = (row: College) => {
   // 这里可以添加编辑逻辑
+  college.value = row;
+  isShow.value = true;
 };
 
-// 删除考试
-const handleDelete = async (_: number) => {
+const handleCollegeDelete = async (_: number) => {
   try {
     ElMessage.success('暂不允许删除院校');
+    // await fetchColleges();
+  } catch (error) {
+    ElMessage.error('删除失败');
+  }
+};
+
+const subjectForm = ref({
+  code: '',
+  name: ''
+});
+
+// 添加科目的方法
+function addSubject() {
+  // 这里可以添加前端验证
+  if (!subjectForm.value.code || !subjectForm.value.name) {
+    alert("请填写完整的科目信息");
+    return;
+  }
+  // 假设添加科目到subjects数组
+}
+
+const handleSubjectDelete = async (_: number) => {
+  try {
+    ElMessage.success('暂不允许删除科目');
     // await fetchColleges();
   } catch (error) {
     ElMessage.error('删除失败');
